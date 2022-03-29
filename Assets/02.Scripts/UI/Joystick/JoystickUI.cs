e650ab9b956975d71a9cdbaaa4415dc54f46fdb3
이현재 <qazx5024@gmail.com>
2022-03-30 00:54:11 +0900

    update ui

--- Assets/02.Scripts/UI/Joystick/JoystickUI.cs
+++ Assets/02.Scripts/UI/Joystick/JoystickUI.cs
@@ -16,48 +16,44 @@ public class JoystickUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler,
 
     [SerializeField] private float _radius;
     [SerializeField] private Canvas _canvas;
-    [SerializeField] private Transform _base;
-    [SerializeField] private Transform _stick;
+    [SerializeField] private RectTransform _base;
+    [SerializeField] private RectTransform _stick;
     private bool _inRange;
     private bool _pressed;
     private JoystickEventChannelSO _joystickEventChannel;
 
-    private void Awake()
-    {
-        _base = transform.GetChild(0).GetChild(0);
-        _stick = transform.GetChild(0).GetChild(1);
-    }
-
     private void Update()
     {
-        if (_pressed) JoystickEventChannel.OnDrag(_canvas.InverseTransformDirection(_stick.position - _base.position));
+        if (_pressed) JoystickEventChannel.OnDrag(_stick.anchoredPosition - _base.anchoredPosition);
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         _pressed = true;
 
-        Vector3 position = _canvas.TransformScreenPoint(eventData.position);
+        Vector3 position = eventData.position - _base.anchoredPosition;
+
+        Debug.Log(position);
 
         // check pressed position is in range
-        _inRange = InRange(position, _base.position, _radius);
+        _inRange = InRange(position, _base.anchoredPosition, _radius);
 
         // if in range, set stick's initial position and invoke event
         if (_inRange)
         {
-            _stick.position = position;
-            JoystickEventChannel.OnBeginDrag(_canvas.InverseTransformDirection( _stick.position - _base.position));
+            _stick.anchoredPosition = position;
+            JoystickEventChannel.OnBeginDrag(_stick.anchoredPosition - _base.anchoredPosition);
         }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        Vector3 position = _canvas.TransformScreenPoint(eventData.position);
+        Vector2 position = eventData.position - _base.anchoredPosition;
 
         // check boundary double click
-        if (eventData.clickCount == 2 && !InRange(position, _base.position, _radius))
+        if (eventData.clickCount == 2 && !InRange(position, _base.anchoredPosition, _radius))
         {
-            JoystickEventChannel.OnBoundaryDoubleClicked(_canvas.InverseTransformDirection(position - _base.position));
+            JoystickEventChannel.OnBoundaryDoubleClicked(position - _base.anchoredPosition);
         }
     }
 
@@ -66,25 +62,25 @@ public class JoystickUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler,
         _pressed = false;
 
         // reset stick's position and invoke event
-        _stick.position = _base.position;
+        _stick.anchoredPosition = _base.anchoredPosition;
         JoystickEventChannel.OnEndDrag();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Vector3 position = _canvas.TransformScreenPoint(eventData.position);
+        Vector2 position = eventData.position - _base.anchoredPosition;
 
         // set stick's position
-        if (_inRange) _stick.position = Bound(position, _base.position, _radius);
+        if (_inRange) _stick.anchoredPosition = Bound(position, _base.anchoredPosition, _radius);
     }
 
-    private bool InRange(Vector3 position, Vector3 center, float radius)
+    private bool InRange(Vector2 position, Vector2 center, float radius)
     {
         // check position is in radius from center
         return (position - center).sqrMagnitude < radius * radius;
     }
 
-    private Vector3 Bound(Vector3 position, Vector3 center, float radius)
+    private Vector3 Bound(Vector2 position, Vector2 center, float radius)
     {
         // bound stick's position not to get out of base of joystick
         return InRange(position, center, radius) ? position : center + (position - center).normalized * radius;
--- Assets/02.Scripts/Utility/CanvasExtension.cs
+++ Assets/02.Scripts/Utility/CanvasExtension.cs
@@ -11,9 +11,6 @@ public static class CanvasExtension
             Debug.LogWarning("it doesn't mean anything with overlay mode");
         }
 
-        Debug.Log(((RectTransform)canvas.transform).sizeDelta);
-        Debug.Log(((RectTransform)canvas.transform).rect.size);
-
         Vector3 transformed = canvas.transform.TransformPoint(vec);
         transformed -= canvas.transform.TransformDirection((Vector3)((RectTransform)canvas.transform).sizeDelta / 2 * canvas.transform.localScale.x);
         return transformed;
